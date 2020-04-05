a70009c038490229baf67cb76b0fb1277ff8ae4a
Eric Jensen <magnesium38@gmail.com>
2020-04-05 12:55:09 -0500

    add sentry

---
+++ src/sentry.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
+
+// if (process.env.NODE_ENV === 'production') {
+	Sentry.init({
+		dsn: 'https://92ec602d98c84f0fbad8ac707be017ec@sentry.io/5189508',
+		integrations: [
+			new Integrations.Vue({
+				Vue,
+				attachProps: true,
+				logErrors: true,
+			}),
+		],
+	})
+// }
