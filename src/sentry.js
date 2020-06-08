720eb144292768d6884f3a1972928f8ad06f2139
Eric Jensen <magnesium38@gmail.com>
2020-06-08 08:23:13 -0500

    update sentry

--- src/sentry.js
+++ src/sentry.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import * as Sentry from '@sentry/browser'
 import * as Integrations from '@sentry/integrations'
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	Sentry.init({
 		dsn: 'https://92ec602d98c84f0fbad8ac707be017ec@sentry.io/5189508',
 		integrations: [
@@ -13,4 +13,4 @@ import * as Integrations from '@sentry/integrations'
 			}),
 		],
 	})
-// }
+}
